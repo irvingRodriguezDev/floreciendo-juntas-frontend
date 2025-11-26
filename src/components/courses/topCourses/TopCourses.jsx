c5626b8797d1fd4bbda324425b5f70b33387bd4c
irving rodriguez <al221810739@gmail.com>
2025-11-26 11:59:09 -0600

    fix design top 10

--- src/components/courses/topCourses/TopCourses.jsx
+++ src/components/courses/topCourses/TopCourses.jsx
@@ -1,74 +1,32 @@
-import React, { useContext, useEffect } from "react";
-import "./TopCourses.css"; // aquí guardaremos los estilos extra
+import { useContext, useEffect } from "react";
+import "./TopCourses.css";
 import { Box, Grid, Typography, useTheme } from "@mui/material";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+
 import CoursesContext from "../../../context/Courses/CoursesContext";
-import AuthContext from "../../../context/Auth/AuthContext";
-const courses = [
-  {
-    id: 1,
-    title: "French Lover",
-    img: "https://images.pexels.com/photos/7470497/pexels-photo-7470497.jpeg",
-  },
-  {
-    id: 2,
-    title: "Las Guerreras Kpop",
-    img: "https://images.pexels.com/photos/7470497/pexels-photo-7470497.jpeg",
-  },
-  {
-    id: 3,
-    title: "Rut y Booz",
-    img: "https://images.pexels.com/photos/7470497/pexels-photo-7470497.jpeg",
-  },
-  {
-    id: 4,
-    title: "La Llorona",
-    img: "https://images.pexels.com/photos/7470497/pexels-photo-7470497.jpeg",
-  },
-  {
-    id: 5,
-    title: "Sonic 3",
-    img: "https://images.pexels.com/photos/7470497/pexels-photo-7470497.jpeg",
-  },
-  {
-    id: 6,
-    title: "Titanes del Pacífico",
-    img: "https://images.pexels.com/photos/7470497/pexels-photo-7470497.jpeg",
-  },
-  {
-    id: 7,
-    title: "Titanes del Pacífico",
-    img: "https://images.pexels.com/photos/7470497/pexels-photo-7470497.jpeg",
-  },
-  {
-    id: 8,
-    title: "Titanes del Pacífico",
-    img: "https://images.pexels.com/photos/7470497/pexels-photo-7470497.jpeg",
-  },
-];
+import { Link } from "react-router-dom";
 
 const TopCourses = () => {
   const { getTopTenCourses, topCourses } = useContext(CoursesContext);
-  const { usuario } = useContext(AuthContext);
   const theme = useTheme();
+
   useEffect(() => {
     getTopTenCourses();
   }, []);
-  console.log(topCourses);
 
   return (
     <Box
-      container
-      spacing={2}
       sx={{
-        py: { xs: 6, md: 10 },
+        py: { xs: 6, md: 8 },
         px: { xs: 2, md: 4 },
         background: `
-      linear-gradient(180deg, #fff 0%, #FFF6F8 100%),
+      linear-gradient(180deg, #fff 0%, #FFDFEF 100%),
       radial-gradient(circle at top left, rgba(255,200,220,0.25), transparent 60%)
     `,
-        justifyContent: "center",
         borderRadius: "24px",
       }}
     >
@@ -131,26 +89,105 @@ const TopCourses = () => {
           >
             Cursos Top
           </Box>{" "}
-          10
         </Typography>
       </motion.div>
-      <div className='top-movies-container'>
-        <div className='movies-list'>
-          {topCourses.map((course, index) => (
-            <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={index}>
-              <Box key={index} className='movie-card'>
-                <span className='movie-rank'>{index + 1}</span>
-                <img
-                  src={course.cover_image_url}
-                  alt={course.title}
-                  className='movie-img'
-                />
-                <p className='movie-title'>{course.title}</p>
+
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          padding: "20px",
+        }}
+      >
+        {topCourses.map((course, index) => (
+          <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 3 }} key={index}>
+            <Link
+              to={`/detalle-curso/${course.courseId}`}
+              style={{ textDecoration: "none" }}
+            >
+              <Box
+                className='nf-card-container'
+                sx={{
+                  position: "relative",
+                  borderRadius: "12px",
+                  overflow: "hidden",
+                  // boxShadow: "0px 4px 12px rgba(0,0,0,0.1)",
+                  background: "transparent",
+                }}
+              >
+                <Grid container spacing={2}>
+                  <Grid size={4} sx={{ backgroundColor: "transparent" }}>
+                    <Box
+                      className='nf-number'
+                      sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: { xs: -90, sm: -70, md: -60, lg: -50, xl: -90 },
+                        background: "transparent",
+                        color: "transparent",
+                        width: "100%",
+                        height: "100%",
+                        borderRadius: "50%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        fontWeight: "bold",
+                        fontSize: {
+                          xs: "190px",
+                          sm: "160px",
+                          md: "170px",
+                          lg: "185px",
+                          xl: "200px",
+                        },
+                        zIndex: 2,
+                        transition: "transform 0.3s",
+                        "&:hover": {
+                          transform: "scale(1.05)",
+                        },
+                      }}
+                    >
+                      {index + 1}
+                    </Box>
+                  </Grid>
+                  <Grid
+                    size={8}
+                    sx={{
+                      zIndex: 2,
+                      borderRadius: "16px",
+                      display: "flex",
+                      justifyContent: "center",
+                      justifyItems: "center",
+                    }}
+                  >
+                    <img
+                      src={course.cover_image_url}
+                      alt={course.title}
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        borderRadius: "16px",
+                      }}
+                    />
+                  </Grid>
+                  <Grid
+                    size={12}
+                    sx={{
+                      bg: "white",
+                      textAlign: "center",
+                      textTransform: "uppercase",
+                      color: "#EA4F9B",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    <Typography fontWeight='bold'>{course.title}</Typography>
+                  </Grid>
+                </Grid>
               </Box>
-            </Grid>
-          ))}
-        </div>
-      </div>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
