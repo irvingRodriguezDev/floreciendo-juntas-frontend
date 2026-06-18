217554e2ee74329d84d2adff2a5f1dab90f3739c
DiegoFerrusca <al222110732@gmail.com>
2026-06-18 13:52:38 -0600

    Add react quill to the add post function

--- src/components/Community/PostCard.jsx
+++ src/components/Community/PostCard.jsx
@@ -11,16 +11,25 @@ import {
 } from "@mui/material";
 import { useContext, useState } from "react";
 import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
+import PushPinIcon from "@mui/icons-material/PushPin";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import DOMPurify from "dompurify";
+
 import PostMediaSwiper from "./PostMediaSwipper";
 import CommentItem from "./CommentItem";
 import MessageIcon from "../icons/MessageIcon";
 import CommentComposer from "./CommentComposer";
 import CommunityContext from "../../context/Community/CommunityContext";
-import PushPinIcon from "@mui/icons-material/PushPin";
+
 dayjs.extend(relativeTime);
 dayjs.locale("es");
+
+const isHtmlContent = (text) => {
+  if (!text) return false;
+
+  return /<\/?[a-z][\s\S]*>/i.test(text);
+};
 
 const PostCard = ({ post }) => {
   const { createToogleReaction } = useContext(CommunityContext);
@@ -31,7 +40,7 @@ const PostCard = ({ post }) => {
       sx={{
         borderRadius: "20px",
         boxShadow: "0 12px 32px rgba(0,0,0,0.08)",
-        position: "relative", // 👈 ESTO ES CLAVE
+        position: "relative",
         overflow: "hidden",
       }}
     >
@@ -52,14 +61,13 @@ const PostCard = ({ post }) => {
           <PushPinIcon sx={{ color: "#D72E7A", fontSize: "24px" }} />
         </Box>
       )}
+
       <CardHeader
         avatar={<Avatar src={post.user?.profileImage} />}
         title={
-          <>
-            <Typography fontWeight={700} color='#D72E7A'>
-              {post.user.name}
-            </Typography>
-          </>
+          <Typography fontWeight={700} color='#D72E7A'>
+            {post.user?.name}
+          </Typography>
         }
         subheader={
           <Typography fontSize='0.75rem' color='text.secondary'>
@@ -73,13 +81,102 @@ const PostCard = ({ post }) => {
           {post.title}
         </Typography>
 
-        <Typography mb={2}>{post.content}</Typography>
+        {/* CONTENIDO */}
+        {isHtmlContent(post.content) ? (
+          <Box
+            mb={2}
+            sx={{
+              wordBreak: "break-word",
 
-        {post.media && <PostMediaSwiper media={post.media} />}
+              "& p": {
+                margin: "0 0 10px 0",
+              },
+
+              "& h1": {
+                fontSize: "2rem",
+                fontWeight: 700,
+                margin: "12px 0",
+              },
+
+              "& h2": {
+                fontSize: "1.6rem",
+                fontWeight: 700,
+                margin: "10px 0",
+              },
+
+              "& h3": {
+                fontSize: "1.3rem",
+                fontWeight: 700,
+                margin: "8px 0",
+              },
+
+              "& ul, & ol": {
+                paddingLeft: "24px",
+                marginBottom: "10px",
+              },
+
+              "& li": {
+                marginBottom: "4px",
+              },
+
+              "& blockquote": {
+                borderLeft: "4px solid #D72E7A",
+                paddingLeft: "12px",
+                margin: "10px 0",
+                color: "#666",
+                fontStyle: "italic",
+              },
+
+              "& a": {
+                color: "#1976d2",
+                textDecoration: "underline",
+              },
+
+              "& img": {
+                maxWidth: "100%",
+                borderRadius: "8px",
+              },
+
+              "& .ql-align-center": {
+                textAlign: "center",
+              },
+
+              "& .ql-align-right": {
+                textAlign: "right",
+              },
+
+              "& .ql-align-justify": {
+                textAlign: "justify",
+              },
+            }}
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(post.content, {
+                ADD_ATTR: ["target"],
+              }).replace(
+                /<a /g,
+                '<a target="_blank" rel="noopener noreferrer" '
+              ),
+            }}
+          />
+        ) : (
+          <Typography
+            mb={2}
+            sx={{
+              whiteSpace: "pre-wrap",
+              wordBreak: "break-word",
+            }}
+          >
+            {post.content}
+          </Typography>
+        )}
+
+        {post.media?.length > 0 && (
+          <PostMediaSwiper media={post.media} />
+        )}
 
         {/* Acciones */}
         <Stack direction='row' spacing={3} alignItems='center' sx={{ mt: 1 }}>
-          {/* ❤️ Likes */}
+          {/* Likes */}
           <Stack direction='row' spacing={0.5} alignItems='center'>
             <IconButton
               onClick={() => createToogleReaction(post.id)}
@@ -106,12 +203,15 @@ const PostCard = ({ post }) => {
             </Typography>
           </Stack>
 
-          {/* 💬 Comentarios */}
+          {/* Comentarios */}
           <Stack
             direction='row'
             spacing={0.5}
             alignItems='center'
-            sx={{ cursor: "pointer", userSelect: "none" }}
+            sx={{
+              cursor: "pointer",
+              userSelect: "none",
+            }}
             onClick={() => setShowComments((prev) => !prev)}
           >
             <IconButton
@@ -146,7 +246,10 @@ const PostCard = ({ post }) => {
 
             <Stack spacing={2} mb={2}>
               {post.comments?.map((comment) => (
-                <CommentItem key={comment.id} comment={comment} />
+                <CommentItem
+                  key={comment.id}
+                  comment={comment}
+                />
               ))}
             </Stack>
 
